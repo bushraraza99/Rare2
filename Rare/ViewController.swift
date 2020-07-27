7f4a6abb03aa12462962a8318aa34983d8eb7509
Trisha Gannu <gannutrisha@gmail.com>
2020-07-27 15:01:24 -0500

    Initial Commit

---
+++ Rare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rare
+//
+//  Created by Trisha Gannu on 7/27/20.
+//  Copyright © 2020 Trisha Gannu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
