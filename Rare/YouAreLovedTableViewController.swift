2170cff2c9e17fa24bf71ec6836fe987c8ef56e4
Isabel Ung <itssewisabel@gmail.com>
2020-07-28 14:06:25 -0500

    ..

--- Rare/YouAreLovedTableViewController.swift
+++ Rare/YouAreLovedTableViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class YouAreLovedTableViewController: UITableViewController {
 
+    @IBOutlet weak var tikTok: UIWebView!;var (_, sender: UIButton) {
+        let URL = NSURL(string: "https://www.tiktok.com/@jazzthornton_/video/6814304604315028742?_d=secCgsIARCbDRgBIAIoARI%2BCjzV405xpbpmfd3MZVqorbzJRSRYyF3N2QcGKZPKfUfmG5VFwxWgcasZPmRExTjsKwJojWkPVDU%2FmXsIpM4aAA%3D%3D&language=en&preview_pb=0&share_item_id=6814304604315028742&timestamp=1595864717&tt_from=copy&u_code=dbh0hd0khlc1aa&user_id=6809762980525048838&utm_campaign=client_share&utm_medium=ios&utm_source=copy&source=h5_m")
+        UIApplication.shared.openURL(URL! as URL)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
