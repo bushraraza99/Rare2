05f046bcfe43f0de2160aec3d5d8fea48838a73c
Trisha Gannu <gannutrisha@gmail.com>
2020-07-29 14:39:15 -0500

    new

---
+++ Rare/JournalViewController.swift
@@ -0,0 +1,34 @@
+//
+//  JournalViewController.swift
+//  Rare
+//
+//  Created by Trisha Gannu on 7/29/20.
+//  Copyright © 2020 Trisha Gannu. All rights reserved.
+//
+
+import UIKit
+
+class JournalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func journalButton(_ sender: Any){ if let url = URL(string: "https://www.hackingwithswift.com") {     UIApplication.shared.open(url) }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
+}
