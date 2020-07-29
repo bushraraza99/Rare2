d5c0e7059c4321685185683f8a38d4b8b5121cca
Bushra Raza <bushraraza99@gmail.com>
2020-07-29 16:52:55 -0500

    hotlines

---
+++ Rare/ViewControllerHotline.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerHotline.swift
+//  Rare
+//
+//  Created by Ruhi Raza on 7/29/20.
+//  Copyright © 2020 Trisha Gannu. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerHotline: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
